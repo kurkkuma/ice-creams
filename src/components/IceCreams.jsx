b135238a3c3a78c3cf2aa48ce7339d7fa19a2a8e
Alina Vetchanina <vetchanina.alina@gmail.com>
2023-04-16 15:04:42 +0300

    main screen

--- src/components/IceCreams.jsx
+++ src/components/IceCreams.jsx
@@ -1,9 +1,37 @@
-import React, { useContext } from "react";
-import { AppContext } from "../App";
+import React from "react";
 
-function IceCream() {
-  const { iceCreams } = useContext(AppContext);
-  return <div>{iceCreams[0].name}</div>;
+function IceCream({
+  name,
+  description,
+  weight,
+  calories,
+  fatContent,
+  price,
+  img,
+}) {
+  return (
+    <div className="wrapper">
+      <div className="info">
+        <h2 className="name">{name}</h2>
+        <p className="description">{description}</p>
+        <button className="add-to-basket">
+          Add to basket{" "}
+          <img
+            className="basket-btn"
+            src="../../images/icons/basket.png"
+            alt=""
+          />
+        </button>
+      </div>
+      <img className="ice-cream-img" src={`../.${img}`} alt="" />
+      <div className="composition">
+        <h3 className="weight">{weight}g</h3>
+        <h3 className="calories">{calories}cal</h3>
+        <h3 className="fat-content">{fatContent}%</h3>
+        <h3 className="price">{price}$</h3>
+      </div>
+    </div>
+  );
 }
 
 export default IceCream;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../App";
+
+import IceCream from "./IceCreams";
 
 function Main() {
+  const { iceCreams } = useContext(AppContext);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePrev = () => {
+    setActiveIndex(activeIndex === 0 ? iceCreams.length - 1 : activeIndex - 1);
+  };
+  const handleNext = () => {
+    setActiveIndex(activeIndex === iceCreams.length - 1 ? 0 : activeIndex + 1);
+  };
+
   return (
     <div className="main">
       <nav className="nav">
@@ -10,11 +24,48 @@ function Main() {
           <img className="star2" src="./images/icons/star-left.png" alt="" />
           Make own ice cream
         </button>
-
         <button className="btn-basket">
           <img src="./images/icons/basket.png" alt="" />
         </button>
       </nav>
+
+      <div onClick={handlePrev} className="left">
+        <img src="../../images/icons/left.png" alt="" />
+      </div>
+      <div onClick={handleNext} className="right">
+        <img className="right" src="../../images/icons/right.png" alt="" />
+      </div>
+
+      {iceCreams.map((ice, index) => {
+        if (activeIndex === index) {
+          return (
+            <IceCream
+              key={index}
+              name={ice.name}
+              description={ice.description}
+              weight={ice.weight}
+              calories={ice.calories}
+              fatContent={ice.fatContent}
+              price={ice.price}
+              img={ice.img}
+            />
+          );
+        }
+      })}
+
+      <div className="dots">
+        {iceCreams.map((_, index) => {
+          return (
+            <span
+              onClick={() => setActiveIndex(index)}
+              className={`dot ${activeIndex === index ? "active" : ""}`}
+              key={index}
+            >
+              ●
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
